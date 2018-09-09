ee26c2d0411485365ae675f948849b70f3a86c96
Lady Rios <lady091217@gmail.com>
2018-09-08 20:13:55 -0500

    menu

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,7 +49,11 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
+            'document' => 'required|string|max:11',
+            'name' => 'required|string|max:45',
+            'lastname' => 'required|string|max:45',
+            'adress' => 'required|string|max:45',
+            'phone' => 'required|string|max:',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
